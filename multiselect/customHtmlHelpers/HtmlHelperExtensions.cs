c633381632073c721f36962674f6ca3f23e99de9
William Alexander Brito Vinas <softwalex74@gmail.com>
2019-05-13 20:25:44 -0400

    Html composition code almost complete

--- multiselect/customHtmlHelpers/HtmlHelperExtensions.cs
+++ multiselect/customHtmlHelpers/HtmlHelperExtensions.cs
@@ -23,13 +23,23 @@ namespace CustomHtmlHelpers {
             
             IEnumerable<T> results = ExpressionMetadataProvider.FromLambdaExpression(expression, htmlHelper.ViewData, htmlHelper.MetadataProvider).Model as IEnumerable<T>;
 
-
             TagBuilder mainContainer = new TagBuilder("div");
             foreach(PropertyInfo prop in htmlAttributes.GetType().GetProperties()) {
                 mainContainer.Attributes.Add(prop.Name, prop.GetValue(htmlAttributes).ToString());
             }
 
+            int totalRows = items.Count() / cols + 1;
+            TagBuilder grid = new TagBuilder("div");
+            grid.Attributes.Add("style", $"grid-template-columns: {cols}; grid-template-rows: {totalRows}");
+
+            int index = 0;
             foreach(SelectListItem item in items) {
+                int currentCol = index % cols + 1;
+                int currentRow = index / cols + 1;
+
+                TagBuilder cell = new TagBuilder("div");
+                cell.Attributes.Add("style", $"grid-column-start: {currentCol}; grid-row-start: {currentRow}");
+
                 TagBuilder checkbox = new TagBuilder("input");
                 Dictionary<string, string> attributes = new Dictionary<string, string>() {
                     { "type", "checkbox" },
@@ -43,9 +53,22 @@ namespace CustomHtmlHelpers {
 
                 TagBuilder span = new TagBuilder("span");
                 span.InnerHtml.AppendLine(item.Text);
+
+                cell.InnerHtml.AppendLine(checkbox.RenderSelfClosingTag());
+                cell.InnerHtml.AppendHtml(span.RenderStartTag());
+                cell.InnerHtml.AppendHtml(span.RenderBody());
+                cell.InnerHtml.AppendLine(span.RenderEndTag());
+
+                grid.InnerHtml.AppendHtml(cell.RenderStartTag());
+                grid.InnerHtml.AppendHtml(cell.RenderBody());
+                grid.InnerHtml.AppendLine(cell.RenderEndTag());
+
+                index++;
             }
 
-
+            mainContainer.InnerHtml.AppendHtml(grid.RenderStartTag());
+            mainContainer.InnerHtml.AppendHtml(grid.RenderBody());
+            mainContainer.InnerHtml.AppendLine(grid.RenderEndTag());
 
             return null;
         }
